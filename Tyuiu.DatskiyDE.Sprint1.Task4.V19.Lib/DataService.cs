47bff3496380c616b330c4ab9aa17e375da58813
BillyHarrington1 <datskiydenis@gmail.com>
2025-09-20 03:43:35 +0500

    Fix Task4

--- Tyuiu.DatskiyDE.Sprint1.Task4.V19.Lib/DataService.cs
+++ Tyuiu.DatskiyDE.Sprint1.Task4.V19.Lib/DataService.cs
@@ -6,9 +6,14 @@ namespace Tyuiu.DatskiyDE.Sprint1.Task4.V19.Lib
     {
         public double Calculate(double x, double y)
         {
-            var res = x + y / Math.Abs(x - 2);
-            res = Math.Round(res, 3);           
-            return res;
+            if (Math.Abs(x - 2) < 0.0001) 
+            {
+                
+            }
+            double numerator = x + y;
+            double denominator = Math.Abs(x - 2);
+            double result = numerator / denominator;
+            return Math.Round(result, 3);           
         }                  
     }
 }
